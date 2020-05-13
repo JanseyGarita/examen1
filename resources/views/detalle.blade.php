58766ec2863f712309e58132cc1458d92c9c6c76
Armando-Madriz <armandoms1578@gmail.com>
2020-05-13 10:30:07 -0600

    Cambios en prueba y detalle
    cambios en prueba y detalle para ver la información

--- resources/views/detalle.blade.php
+++ resources/views/detalle.blade.php
@@ -1,24 +1,50 @@
 @extends('layout.layout')
 
 @section('links')
-
+<link rel="stylesheet" href="styles/profile.css">
 @endsection
 
 @section('content')
 
-<div class="container body-content">
+<svg viewBox="0 0 1440 320" style="position: relative; top: 0; z-index: 0;">
+    <path fill="#00695c" fill-opacity="1"
+        d="M0,224L120,234.7C240,245,480,267,720,272C960,277,1200,267,1320,261.3L1440,256L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z">
+    </path>
+</svg>
 
-      <h1 scope="row">{{$profile->id}}</h1>
-
-            <h2>{{$profile->user_name}}</h2>
-
-      <h3>{{$profile->mail}}</h3>
-      <h4>{{$profile->phone}}</h4>
-
+<div class="container info-card body-content">
+    <div class="row flex-center" style="flex-direction: column;">
+        <h3>Mi Información</h3>
+        <form class="col s12">
+            <div class="row">
+                <div class="input-field col s6">
+                    <input id="user_name" type="text" class="validate" name="user_name" required value="{{$profile->user_name}}">
+                    <label for="user_name">Nombre</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="phone_number" type="tel" class="validate" name="phone_number" required value="{{$profile->phone}}">
+                    <label for="phone_number">Teléfono</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s6">
+                    <input id="email" type="email" class="validate" name="mail" required value="{{$profile->mail}}">
+                    <label for="email">Email</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="password" type="password" class="validate" name="user_password" required value="{{$profile->user_password}}">
+                    <label for="password">Password</label>
+                </div>
+            </div>
+            <button class="btn waves-effect waves-light" type="submit" name="action">Guardar
+                <i class="material-icons right">save</i>
+            </button>
+        </form>
+    </div>
 </div>
 
 @endsection
 
 @section('scripts')
-    
+
 @endsection
--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -3,34 +3,51 @@
 @section('content')
 
 <div class="container body-content">
-<table class="table highlight">
-  <thead>
-    <tr>
-      <th scope="col">#id</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Correo</th>
-      <th scope="col">Teléfono</th>
-    </tr>
-  </thead>
-  <tbody>
-  @foreach($profiles as $item)
-    <tr>
-      <th scope="row">{{$item->id}}</th>
-        <td>
-          <a href="{{ route('detalle', $item) }}">
-            {{$item->user_name}}
-          </a>
-        </td>
-      <td>{{$item->mail}}</td>
-      <td>{{$item->phone}}</td>
-    </tr>
-    @endforeach()
-  </tbody>
-</table>
+    <div class="row flex-center m-2">
+        <div class="card" style="width: 100%;">
+            <div class="card-body flex-center" style="flex-direction: column;">
+                <h5 style="margin-top: 1em">Buscar usuarios</h5>
+                <div class="row container">
+                    <div class="input-field col s9 m10">
+                        <input type="text" id="autocomplete-input" class="autocomplete">
+                        <label for="autocomplete-input">Nombre del usuario</label>
+                    </div>
+                    <div class="col s3 m2">
+                        <a class="btn-floating btn-medium waves-effect waves-light mt-2"">
+                            <i class="material-icons prefix">search</i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row users-container">
+    @foreach($profiles as $item)
+        <div class="col s12 m6 l4">
+            <div class="card" style="padding: 1em 0 2em 0;">
+                <div class="card-body flex-center" style="flex-direction: column; padding: 0.5em; text-align: center;">
+                    <h5 style="padding-bottom: 0.4em;">{{$item->user_name}}</h5>
+                    <a class="waves-effect waves-light btn-small"  href="{{ route('detalle', $item) }}">
+                        <i class="material-icons left">remove_red_eye</i>
+                        Ver
+                    </a>
+                </div>
+            </div>
+        </div>
+        @endforeach()
+    </div>
 </div>
 
 @endsection
 
 @section('scripts')
-    
+<script>
+    $list = null;
+
+    $(document).ready(function(){
+    $('input.autocomplete').autocomplete({
+      data: $list
+    });
+  });
+</script>
 @endsection
