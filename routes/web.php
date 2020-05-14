51777f5e9af82ec2ee3cb4ef99cded37237c067d
JanseyGarita <janseygarita@gmail.com>
2020-05-13 21:25:13 -0600

    Seguridad agregada
    Se termino de agregar la fuerza bruta a las rutas, pero la ruta /profile tira un error desconocido.

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'routes_controller@get_profile')->name('profile');
-Route::get('/community', 'routes_controller@get_profiles');
-Route::get('/watch', 'routes_controller@get_videos');
+//(isset($_COOKIE['user'])? '':'routes_controller@get_login')
 
-Route::post('/profile', 'profile_controller@update')->name('update');
+Route::get('/', (isset($_COOKIE['user']) ? 'routes_controller@get_profile' : 'routes_controller@get_login'))->name('profile');
 
-Route::post('/profile/{id?}', 'profile_controller@get_view_profile')->name('view_profile');
+Route::get('/community', (isset($_COOKIE['user']) ? 'routes_controller@get_profiles' : 'routes_controller@get_login'));
 
-Route::get('/login', (!isset($_COOKIE['user'])? 'routes_controller@get_login':'routes_controller@get_profile'));
+Route::get('/watch', (isset($_COOKIE['user']) ? 'routes_controller@get_videos' : 'routes_controller@get_login'));
 
-Route::post('/login', 'profile_controller@login')->name('login');
+Route::post('/profile', (isset($_COOKIE['user']) ? 'profile_controller@update' : 'routes_controller@get_login'))->name('update');
 
-Route::get('/exit', 'profile_controller@logout');
+Route::post('/profile/{id?}', (isset($_COOKIE['user']) ? 'profile_controller@get_view_profile' : 'routes_controller@get_login'))->name('view_profile');
+
+Route::get('/login', (!isset($_COOKIE['user']) ? 'routes_controller@get_login' : 'routes_controller@get_profile'));
+
+Route::post('/login', (!isset($_COOKIE['user']) ? 'profile_controller@login' : 'routes_controller@get_login'))->name('login');
+
+Route::get('/exit', (isset($_COOKIE['user']) ? 'profile_controller@logout' : 'routes_controller@get_login'));
